d1319bffeccf8d82acd949de9590da97e6b52eec
Priyanka K <54153547+thepriyankakashyap@users.noreply.github.com>
2022-10-30 13:56:31 +0530

    code for playing music in python

---
+++ air_drum_set/play_music.py
@@ -0,0 +1,8 @@
+### pip install playsound
+
+# import required module
+from playsound import playsound
+  
+# for playing note.wav file
+playsound('/path/note.wav')
+print('playing sound using  playsound')
